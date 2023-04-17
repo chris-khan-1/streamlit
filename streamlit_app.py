3c7aa8433ba452f165d432591d22dc6aaf39ae68
chris <ckhan46.ck@gmail.com>
2023-04-17 22:39:18 +0100

    numeric

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,8 @@ for i in ["SPR"]:
 
 b = pd.concat([pd.DataFrame(x).T for x in dicts]).reset_index()
 
-spr_pos = b[b["index"].str.contains("position")].fillna(25)
-spr_points = b[b["index"].str.contains("points")].fillna(0)
+spr_pos = b[b["index"].str.contains("position")].fillna(25).to_numeric()
+spr_points = b[b["index"].str.contains("points")].fillna(0).to_numeric()
 
 st.write(spr_pos.dtypes)
 st.write(spr_points.dtypes)
