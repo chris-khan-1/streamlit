5da09308e66ba65953d786336222af5e1cb2058a
chris <ckhan46.ck@gmail.com>
2023-05-01 00:16:26 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -157,9 +157,9 @@ fig1 = px.line(
             )
 
 fig1['layout']['yaxis']['autorange'] = "reversed"
+fig1.update_layout(height=800)
 
-
-st.plotly_chart(fig1, theme="streamlit")
+st.plotly_chart(fig1, theme="streamlit", use_container_width=True, height=800)
 
 
 race_dicts = get_results("RAC")
