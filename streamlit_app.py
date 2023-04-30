b6d7ded8f301809b890a226dee011cbb4915cc57
chris <ckhan46.ck@gmail.com>
2023-05-01 00:09:20 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,6 +128,7 @@ else:
     df_final = pd.concat(dfs, axis=1)
 
 df_final["position"] = range(1,len(df_final)+1)
+st.dataframe(df_final)
 # st.dataframe(df_final.reset_index().style.applymap(color_rider))
 st.dataframe(df_final.style.apply(lambda x:['background-color: green' if s==rider else '' for s in x]))
 
