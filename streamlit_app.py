f39cf63fe06242d4b53e671e7592c4e15be7e3ea
chris <ckhan46.ck@gmail.com>
2023-06-06 18:38:45 +0100

    change background colours

--- streamlit_app.py
+++ streamlit_app.py
@@ -159,11 +159,11 @@ df_final = df_final.reindex(sorted(list(df_final.columns), key= lambda x: float(
 
 
 if len(rider) == 1:
-    st.dataframe(df_final.style.apply(lambda x: ['background-color: #004f05' if s == rider[0] else '' for s in x]), use_container_width=True)
+    st.dataframe(df_final.style.apply(lambda x: ['background-color: #008a05' if s == rider[0] else '' for s in x]), use_container_width=True)
 elif len(rider) == 2:
-    st.dataframe(df_final.style.apply(lambda x: ['background-color: #004f05' if s == rider[0] else '' 'background-color: #4f003f' if s == rider[1] else '' for s in x]), use_container_width=True)
+    st.dataframe(df_final.style.apply(lambda x: ['background-color: #008a05' if s == rider[0] else '' 'background-color: #4f003f' if s == rider[1] else '' for s in x]), use_container_width=True)
 elif len(rider) == 3:
-    st.dataframe(df_final.style.apply(lambda x: ['background-color: #004f05' if s == rider[0] else '' 'background-color: #4f003f' if s == rider[1] else '' 'background-color: #273346' if s == rider[2] else ''for s in x]), use_container_width=True)
+    st.dataframe(df_final.style.apply(lambda x: ['background-color: #008a05' if s == rider[0] else '' 'background-color: #4f003f' if s == rider[1] else '' 'background-color: #273346' if s == rider[2] else ''for s in x]), use_container_width=True)
 # st.dataframe(df_final.reset_index().style.applymap(color_rider))
 
 
