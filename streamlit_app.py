100fd340c9b4a51e51571eddc6eea196eb9ad3ce
chris <ckhan46.ck@gmail.com>
2023-06-06 15:21:47 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -151,9 +151,9 @@ else:
     df_final = pd.concat(dfs, axis=1)
 
 # df_final = df_final.reindex(sorted(list(df.columns), key= lambda x: float(x.split('-')[-1])), axis=1)
-df_final = df_final.reindex(sorted(list(df.columns)))
 df_final["Pos."] = range(1, len(df_final)+1)
 df_final.set_index("Pos.", inplace=True)
+df_final = df_final.reindex(sorted(list(df.columns)))
 
 # st.dataframe(df_final.reset_index().style.applymap(color_rider))
 st.dataframe(df_final.style.apply(lambda x: ['background-color: green' if s == rider else '' for s in x]), use_container_width=True)
