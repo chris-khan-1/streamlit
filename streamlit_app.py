fa29679ef5031caf4e4a00800cad94237728f3a1
chris <ckhan46.ck@gmail.com>
2023-10-28 16:32:10 +0100

    tes

--- streamlit_app.py
+++ streamlit_app.py
@@ -198,7 +198,7 @@ def get_championship_table(combined_points):
     c = c.loc[c["rider"] != "index"]
     c.columns = ["Rider", "Points"]
     c = c.sort_values(by="Points", ascending=False)
-    c["Difference"] = [max(c.Points) - c.Points]
+    c["Difference"] = [max(c.Points) - i for i in c.Points].replace(0, "-")
     return c
 
 tracks = {"NED": "Assen (Netherlands)",
