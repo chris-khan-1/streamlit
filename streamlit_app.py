7c6251df761a26977adceeec41af3fd3741d5338
chris <ckhan46.ck@gmail.com>
2023-10-28 14:58:31 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def get_gsheet_creds():
 
     return credentials
 
-# @st.cache_data(show_spinner="Fetching data from API...") #ttl=601800, 
+@st.cache_data(show_spinner="Fetching data from API...") #ttl=601800, 
 def get_gsheet_data(name):
     credentials = get_gsheet_creds()
     file = gspread.authorize(credentials) # authenticate the JSON key with gspread
