f2c48ccfbce34e6a7d7ac6901c964b81845a1958
chris <ckhan46.ck@gmail.com>
2024-03-09 21:10:59 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,9 @@ all_data = get_all_data()
 
 spr_pos, spr_points, rac_pos, rac_points, combined_points, comb_riders, sorted_riders = get_and_transform_current_results(year)
 
+for i in [spr_pos, spr_points, rac_pos, rac_points, combined_points, comb_riders, sorted_riders]:
+    st.write(i)
+
 champ_table = get_championship_table(combined_points)
 
 fantasy_df = get_gsheet_data(f"{year}_fantasy")
