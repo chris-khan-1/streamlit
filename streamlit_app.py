49e4d148cf80a4ae01c0d8c23e348ba187dc230f
chris <ckhan46.ck@gmail.com>
2024-03-11 18:23:50 +0000

    sdfkjsdlkf

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fantasy_df = filter_fantasy_df(fantasy_df).dropna()
 fantasy_teams_df = get_gsheet_data(f"{year}_fantasy_constructors")
 fantasy_teams_df = filter_fantasy_teams_df(fantasy_teams_df).dropna()
 
-st.dataframe(get_gsheet_data(year).columns[::2][1:].split("_")[0])
+st.dataframe(list(get_gsheet_data(year).columns)[::2][1:])#.split("_")[0])
 # _________________________________________________________________________________________________________________
 # START OF PAGE LAYOUT
 vert_space = '<div style="padding: 25px 5px;"></div>'
